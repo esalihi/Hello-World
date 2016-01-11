8b228125889cd084cda0e3d3d9d617fee861d386
Endrit Salihi <esalihi@student.tgm.ac.at>
2016-01-11 13:00:42 +0100

    1

---
+++ src/salihi/Hello.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package salihi;
+
+/**
+ * @author Hp m6
+ *
+ */
+public class Hello {
+
+	/**
+	 * 
+	 */
+	public Hello() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
